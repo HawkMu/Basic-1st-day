ffc945f11895a4db5ab8bcb98be80182df7d4331
タカムラ カズキ 18TE402 <18te402@ueno.daiichi-koudai.ac.jp>
2022-08-04 19:49:28 +0900

    プロジェクト ファイルを追加します。

---
+++ Sample103/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample103
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 修正前:Console.WriteLime("HelloWorld.");
+            Console.WriteLine("HelloWorld.");
+        }
+    }
+}
